8e665d9cc0d10782ce4f26acb1e5eb928beeb59d
StefanUngureanul24 <unsugreanu_stefan_lucian@yahoo.com>
2022-06-08 07:59:02 +0200

    Adjustment of post method

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,14 +1,22 @@
 const http = require('http');
+const express = require('express');
+const app = express();
 
 const hostname = 'localhost';
 const port = 3001;
 
+/*
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
 });
+*/
 
-server.listen(port, hostname, () => {
+app.post('/user', function(req, res) {
+    console.log(req.body);
+})
+
+app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
