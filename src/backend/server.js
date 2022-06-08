686013ea3d3b6f32408878b591eb45723a029a19
StefanUngureanul24 <unsugreanu_stefan_lucian@yahoo.com>
2022-06-08 08:37:40 +0200

    Add of bodyParser

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+var bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3001;
@@ -16,6 +17,10 @@ const server = http.createServer((req, res) => {
 
 // For data acces - CORS policy
 app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
 
 app.post('', function(req, res) {
     console.log(req.body);
